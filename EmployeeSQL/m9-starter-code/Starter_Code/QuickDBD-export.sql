2bab61c0905c034d76a1b1836914f1261a444d94
Skyler Khalachyan <skyler.khalachyan@gmail.com>
2023-05-29 14:31:53 -0700

    Editing

--- EmployeeSQL/m9-starter-code/Starter_Code/QuickDBD-export.sql
+++ EmployeeSQL/m9-starter-code/Starter_Code/QuickDBD-export.sql
@@ -15,7 +15,7 @@ CREATE TABLE "dept_emp" (
     "dept_emp_id" INT   NOT NULL,
     "emp_no" INT   NOT NULL,
     "dept_no" VARCHAR   NOT NULL,
-    "departments_id" INT   NOT NULL,
+    "department_id" INT   NOT NULL,
     CONSTRAINT "pk_dept_emp" PRIMARY KEY (
         "dept_emp_id"
      )
@@ -23,9 +23,9 @@ CREATE TABLE "dept_emp" (
 
 CREATE TABLE "dept_manager" (
     "dept_manager_id" INT   NOT NULL,
-    "dept_no" varchar   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL,
     "emp_no" INT   NOT NULL,
-    "departments_id" INT   NOT NULL,
+    "department_id" INT   NOT NULL,
     CONSTRAINT "pk_dept_manager" PRIMARY KEY (
         "dept_manager_id"
      )
@@ -40,44 +40,45 @@ CREATE TABLE "employees" (
     "last_name" VARCHAR   NOT NULL,
     "sex" VARCHAR   NOT NULL,
     "hire_date" DATE   NOT NULL,
-    "departments_id" INT   NOT NULL,
+    "department_id" INT   NOT NULL,
     CONSTRAINT "pk_employees" PRIMARY KEY (
-        "employee_id"
+        "employee_id","department_id"
      )
 );
 
 CREATE TABLE "salaries" (
     "salaries_id" INT   NOT NULL,
+    "employee_id" INT   NOT NULL,
     "emp_no" INT   NOT NULL,
     "salary" INT   NOT NULL,
-    "employee_id" INT   NOT NULL,
+    "department_id" INT   NOT NULL,
     CONSTRAINT "pk_salaries" PRIMARY KEY (
-        "salaries_id"
+        "salaries_id","department_id"
      )
 );
 
 CREATE TABLE "Titles" (
-    "titles_id" INT   NOT NULL,
-    "title_id" VARCHAR   NOT NULL,
+    "emp_no" INT   NOT NULL,
+    "department_id" INT   NOT NULL,
+    "emp_title_id" VARCHAR   NOT NULL,
     "title" VARCHAR   NOT NULL,
-    "departments_id" INT   NOT NULL,
     CONSTRAINT "pk_Titles" PRIMARY KEY (
-        "titles_id"
+        "emp_no","department_id"
      )
 );
 
-ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_departments_id" FOREIGN KEY("departments_id")
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_department_id" FOREIGN KEY("department_id")
 REFERENCES "Departments" ("department_id");
 
-ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_departments_id" FOREIGN KEY("departments_id")
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_department_id" FOREIGN KEY("department_id")
 REFERENCES "Departments" ("department_id");
 
-ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_departments_id" FOREIGN KEY("departments_id")
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_department_id" FOREIGN KEY("department_id")
 REFERENCES "Departments" ("department_id");
 
-ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_employee_id" FOREIGN KEY("employee_id")
-REFERENCES "employees" ("employee_id");
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_employee_id_department_id" FOREIGN KEY("employee_id", "department_id")
+REFERENCES "employees" ("employee_id", "department_id");
 
-ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_departments_id" FOREIGN KEY("departments_id")
+ALTER TABLE "Titles" ADD CONSTRAINT "fk_Titles_department_id" FOREIGN KEY("department_id")
 REFERENCES "Departments" ("department_id");
 
